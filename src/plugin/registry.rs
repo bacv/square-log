3d1c6c0ffa4587f2076e64b250be5b577f856cae
Gusto <bacvinka@gmail.com>
2024-01-22 22:45:40 +0200

    Reorganize plugin loading function

--- src/plugin/registry.rs
+++ src/plugin/registry.rs
@@ -1,7 +1,7 @@
-use std::{collections::HashMap, fs, sync::Arc};
+use std::{collections::HashMap, fs, path::PathBuf, sync::Arc};
 
 use color_eyre::eyre::eyre;
-use mlua::{Function, Lua};
+use mlua::{Function, Lua, Result};
 
 use super::{api::Api, PluginConfig, LUA_PLUGIN_CALL_FN, RUST_API_GLOBAL_NAME};
 use crate::source::Source;
@@ -11,34 +11,16 @@ pub struct PluginRegistry {
 }
 
 impl PluginRegistry {
-    pub fn init(config: PluginConfig) -> mlua::Result<Self> {
-        let mut registry = Self {
-            plugins: HashMap::new(),
-        };
-
+    pub fn init(config: PluginConfig) -> Result<Self> {
         let api = Arc::new(Api);
 
+        let mut plugins = HashMap::new();
         for entry in fs::read_dir(config.path)? {
-            let path = entry?.path();
-            if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("lua") {
-                let lua = Lua::new();
-                let script = fs::read_to_string(&path)?;
-                let plugin_name = path
-                    .file_stem()
-                    .and_then(|s| s.to_str())
-                    .ok_or_else(|| {
-                        mlua::Error::RuntimeError("Invalid plugin file name".to_string())
-                    })?
-                    .to_string();
-
-                lua.load(&script).exec()?;
-                lua.globals().set(RUST_API_GLOBAL_NAME, api.clone())?;
-
-                registry.plugins.insert(plugin_name, lua);
-            }
+            let (plugin_name, plugin_source) = load_plugin(entry?.path(), &api)?;
+            plugins.insert(plugin_name, plugin_source);
         }
 
-        Ok(registry)
+        Ok(Self { plugins })
     }
 
     pub async fn call(&self, source: &Source) -> color_eyre::Result<()> {
@@ -51,5 +33,26 @@ impl PluginRegistry {
 
         call_fn.call_async::<_, ()>(source.address.clone()).await?;
         Ok(())
+    }
+}
+
+fn load_plugin(path: PathBuf, api: &Arc<Api>) -> Result<(String, Lua)> {
+    if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("lua") {
+        let lua = Lua::new();
+        let script = fs::read_to_string(&path)?;
+        let plugin_name = path
+            .file_stem()
+            .and_then(|s| s.to_str())
+            .ok_or_else(|| mlua::Error::RuntimeError("Invalid plugin file name".to_string()))?
+            .to_string();
+
+        lua.load(&script).exec()?;
+        lua.globals().set(RUST_API_GLOBAL_NAME, api.clone())?;
+
+        Ok((plugin_name, lua))
+    } else {
+        Err(mlua::Error::RuntimeError(
+            "File is not a Lua script".to_string(),
+        ))
     }
 }
